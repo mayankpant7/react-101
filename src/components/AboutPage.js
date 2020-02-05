e0032255aabca8e84cd58d249e6bab0caa82c87e
mayankpant7 <mayankpant.pant120@gmial.com>
2020-02-06 02:01:24 +0530

    first commit

---
+++ src/components/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>About</h1>
+        <p>This is a paragraph.</p>
+      </>
+    );
+  }
+}
+
+export default AboutPage;
---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import AboutPage from "./AboutPage";
+import HomePage from "./HomePage";
+import Header from "./common/Header";
+
+function App() {
+  function getPage() {
+    const route = window.location.pathname;
+    if (route === "/about") return <AboutPage />;
+    return <HomePage />;
+  }
+  return (
+    <div>
+      <Header />
+      {getPage()}
+    </div>
+  );
+}
+
+export default App;
